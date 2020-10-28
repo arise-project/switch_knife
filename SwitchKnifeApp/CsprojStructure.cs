0ba90666aaf07f9457bd0f93c23a0b3be3e0b4b3
DESKTOP-NP8I2VB\ypizmail <Evgeny.Pirogov@aspose.com>
2020-10-28 14:51:19 +0200

    find in csproj

---
+++ SwitchKnifeApp/CsprojStructure.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SwitchKnifeApp
+{
+    public class CsprojStructure
+    {
+        public void FindFilesNotPresent(string folder, string projectFile)
+        {
+            var project = File.ReadAllText(projectFile);
+            var files = Directory.GetFiles(folder, "*", SearchOption.AllDirectories).Select(f => f.Substring(folder.Length+1));
+            var notFound = files.Where(f => !project.Contains(f, StringComparison.InvariantCultureIgnoreCase)).ToList();
+            notFound.ForEach(Console.WriteLine);
+        }
+    }
+}
--- SwitchKnifeApp/Program.cs
+++ SwitchKnifeApp/Program.cs
@@ -6,7 +6,7 @@ namespace SwitchKnifeApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("map_test_to_class");
+            Console.WriteLine("map_test_to_class|csproj_find_not_present_file");
             var choice = Console.ReadLine();
 
             switch(choice)
@@ -19,7 +19,16 @@ namespace SwitchKnifeApp
                     var e1 = new MapTestToClass();
                     e1.SetArguments(testsFolder, classesFolder);
                     e1.Execute();
-                break;
+                    break;
+                case "csproj_find_not_present_file":
+                    Console.WriteLine("folder:");
+                    var filesFolder = Console.ReadLine();
+                    Console.WriteLine("csproj file:");
+                    var csprojFile = Console.ReadLine();
+                    var e2 = new CsprojStructure();
+                    e2.FindFilesNotPresent(filesFolder, csprojFile);
+                    break;
+                
             }
         }
     }
