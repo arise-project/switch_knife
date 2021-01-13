7cb07b90ec42dca7306649e6090fb855695a9b60
Yevhen Pyrohov <Evgeny.Pirogov@aspose.com>
2021-01-13 21:54:13 +0200

    fix resx mismatch

--- SwitchKnifeApp/Program.cs
+++ SwitchKnifeApp/Program.cs
@@ -10,7 +10,7 @@ namespace SwitchKnifeApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("map_test_to_class|csproj_find_not_present_file|file_remover|csv_splitter|csv-change|csv-validate-keys|csv-merge|char-case|lsh-serach|resx-key|en-words|resx-patch|resx-format|resx-dots|resx-excepts|restore-except|csv-add-col|resx-repeat-seq|css-selectors|page-folders|page-copy|patch-html");
+            Console.WriteLine("map_test_to_class|csproj_find_not_present_file|file_remover|csv_splitter|csv-change|csv-validate-keys|csv-merge|char-case|lsh-serach|resx-key|en-words|resx-patch|resx-format|resx-dots|resx-excepts|restore-except|csv-add-col|resx-repeat-seq|css-selectors|page-folders|page-copy|patch-html|resx-missmatch");
             var choice = Console.ReadLine();
 
             switch(choice)
@@ -217,6 +217,15 @@ namespace SwitchKnifeApp
                     Console.WriteLine("patch:");
                     var patch = Console.ReadLine();
                     new PatchHtml().Execute(folder, operation, patch);
+                    break;
+                case "resx-missmatch":
+                    Console.WriteLine("folder:");
+                    var folder11 = Console.ReadLine();
+                    Console.WriteLine("original file:");
+                    var file11 = Console.ReadLine();
+                    Console.WriteLine("pattern:");
+                    var pattern1 = Console.ReadLine();
+                    new ResxMissmatch().Execute(folder11, file11, pattern1);
                     break;
             }
         }
---
+++ SwitchKnifeApp/ResxMissmatch.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+namespace SwitchKnifeApp
+{
+    public class ResxMissmatch
+    {
+        public void Execute(string folder, string originalFile, string pattern)
+        {
+            var files = Directory.GetFiles(folder, "*.resx", SearchOption.AllDirectories);
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(originalFile);
+            XmlNode root = doc.DocumentElement;
+            if(string.IsNullOrEmpty(pattern))
+            {
+                pattern = @"(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+            }
+            var urlPattern = new Regex(pattern, RegexOptions.Compiled);
+            foreach (var file in files)
+            {
+                XmlDocument localeDoc = new XmlDocument();
+                localeDoc.Load(file);
+                XmlNode localeRoot = localeDoc.DocumentElement;
+
+
+                XmlNodeList nodes = root.SelectNodes($"descendant::data");
+                for (int i = 0; i < nodes.Count; i++)
+                {
+                    var node = nodes[i];
+                    var key = node.Attributes["name"].Value;
+                    if (node.ChildNodes.Count != 3 || node.ChildNodes[1].ChildNodes.Count != 1)
+                    {
+                        continue;
+                    }
+                    var text1 = node.ChildNodes[1].ChildNodes[0].Value;
+                    var url1 = urlPattern.Match(text1).Value;
+
+                    if(!string.IsNullOrEmpty(url1))
+                    {
+                        XmlNode myNode = localeRoot.SelectSingleNode($"/root/data[@name='{key}']");
+                        if (myNode != null)
+                        {
+                            var text2 = myNode.ChildNodes[1].ChildNodes[0].Value;
+                            var url2 = urlPattern.Match(text2).Value;
+
+                            if (!string.Equals(url1, url2))
+                            {
+                                Console.WriteLine("{0}: '{1}' but {2} - '{3}'", key, url1, Path.GetFileName(file), url2);
+                                if(!string.IsNullOrEmpty(url2))
+                                {
+                                    myNode.ChildNodes[1].ChildNodes[0].Value = text2.Replace(url2, url1);
+                                }
+                            }
+                        }
+                    }
+                }
+                localeDoc.Save(file);
+            }
+        }
+    }
+}
